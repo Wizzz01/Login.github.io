a45b2bebcb3cb99a0e06649537d223440530d31a
Wizzz01 <88884291+Wizzz01@users.noreply.github.com>
2024-03-16 19:46:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 function validateAndSubmit() {
-            const password = document.getElementById('password').value;
+    const password = document.getElementById('passwordReg').value;
 
-            // Check length
-            if (password.length < 8) {
-                alert('Password must be at least 8 characters long');
-                return false;
-            }
+    // Check length
+    if (password.length < 8) {
+        alert('Password must be at least 8 characters long');
+        return false;
+    }
 
-            // Check if consists of only integers
-            if (/^\d+$/.test(password)) {
-                alert('Password must contain at least one letter');
-                return false;
-            }
+    // Check if consists of only integers
+    if (/^\d+$/.test(password)) {
+        alert('Password must contain at least one letter');
+        return false;
+    }
 
-            // Check if combination of uppercase and lowercase characters
-            if (!/[a-z]/.test(password) || !/[A-Z]/.test(password)) {
-                alert('Password must contain at least one uppercase and one lowercase letter');
-                return false;
-            }
+    // Check if combination of uppercase and lowercase characters
+    if (!/[a-z]/.test(password) || !/[A-Z]/.test(password)) {
+        alert('Password must contain at least one uppercase and one lowercase letter');
+        return false;
+    }
 
-            // If successful, submit the form
-            document.querySelector('form').submit();
+    // If successful, submit the form
+    document.getElementById('registrationForm').submit();
 
-            // Show success message
-            alert('Login successful');
+    // Show success message
+    alert('Registration successful');
 
-            return true;
-        }
+    return true;
+}
