d8c3fd90f4783837dfb1e2fc304ed5840ee14f93
Wizzz01 <88884291+Wizzz01@users.noreply.github.com>
2024-03-16 22:23:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,84 @@
-function validateAndSubmit() {
-    const password = document.getElementById('passwordReg').value;
+// For the title
+const title = document.querySelector('#title');
+// For reg form
+const regForm = document.querySelector('.regForm');
 
-    // Check length
-    if (password.length < 8) {
-        alert('Password must be at least 8 characters long');
-        return false;
-    }
+// For reg form fields
+const usernameReg = document.getElementById('usernameReg');
+const passwordReg = document.getElementById('passwordReg');
 
-    // Check if consists of only integers
-    if (/^\d+$/.test(password)) {
-        alert('Password must contain at least one letter');
-        return false;
-    }
+// For login form
+const logForm = document.querySelector('.logForm');
 
-    // Check if combination of uppercase and lowercase characters
-    if (!/[a-z]/.test(password) || !/[A-Z]/.test(password)) {
-        alert('Password must contain at least one uppercase and one lowercase letter');
-        return false;
-    }
+// For login form fields
+const username = document.getElementById('username');
+const password = document.getElementById('password');
 
-    // If successful, submit the form
-    document.getElementById('registrationForm').submit();
+// For username and passwords
+const usernameAndPasswords = {}
 
-    // Show success message
-    alert('Registration successful');
+// For getting the date and time today
+const time = new Date().toLocaleString();
 
-    return true;
+// For checking if a username already exists
+function checkIfUserExists(username, usernameAndPasswords) {
+	if (usernameAndPasswords.hasOwnProperty(username)) {
+		return true
+	}
 }
+
+// For validating username and passwords stored 
+function validateUserNameAndPassword(username, password, usernameAndPasswords,) {
+	if(usernameAndPasswords.hasOwnProperty(username) && usernameAndPasswords[username] == password) {
+		return true;
+	}
+}
+
+regForm.addEventListener('submit', function (e) {
+	e.preventDefault();
+
+	// Validate if one of the fields are empty
+	if(usernameReg.value.length == 0 || passwordReg.value.length == 0) {
+		alert("Fill out all the forms first");
+	}
+
+	// Store username and password to JS object
+	else {
+		if (checkIfUserExists(usernameReg.value, usernameAndPasswords)) {
+			alert('Username is already taken');
+		}
+		else {
+
+			// Store the username and passwords inside the JavaScript Object 
+			usernameAndPasswords[usernameReg.value] = passwordReg.value;
+			console.log(usernameAndPasswords);
+
+			// Display the login form and get rid of the registration form on the page
+			logForm.style.display = "block";
+			regForm.style.display = "none";
+
+		}
+	}
+
+})
+
+logForm.addEventListener('submit', function (e) {
+
+	// Passing username and password to the function
+	if (validateUserNameAndPassword(username.value, password.value, usernameAndPasswords)) {
+
+		// Hide the login form and title after user has been validated
+		logForm.style.display = "none";
+		title.style.display = "none";
+
+		// Greet user who just logged in
+		document.querySelector('.welcomePanel #greeting').innerHTML = "Good day! " + username.value + ". It's currently " + time;
+	}
+	else {
+
+		// Login invalid
+		alert("Username and password don't exist");
+
+	}
+
+})
